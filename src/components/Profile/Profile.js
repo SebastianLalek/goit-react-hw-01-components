fa04d252341af9f4472bc3562a9d40f0add54e42
Sebastian Lalek <chemiczq@gmail.com>
2023-10-15 17:00:50 +0200

    update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,29 @@
-export default function Profile(props) {
+import css from './Profile.module.css';
+
+export default function Profile({ username, tag, location, avatar, stats }) {
   return (
-    <figure className="profile">
-      <img src={props.avatar} alt="user avatar" className="avatar" />
-      <figcaption>
-        <h2 className="name">{props.username}</h2>
-        <p className="tag">{props.tag}</p>
-        <p className="location">{props.location}</p>
-      </figcaption>
-    </figure>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="user avatar" className={css.avatar} />
+
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.stats}>
+        <li>
+          <p className={css.label}>Followers</p>
+          <p className={css.quantity}>{stats.followers}</p>
+        </li>
+        <li>
+          <p className={css.label}>Views</p>
+          <p className={css.quantity}>{stats.views}</p>
+        </li>
+        <li>
+          <p className={css.label}>Likes</p>
+          <p className={css.quantity}>{stats.likes}</p>
+        </li>
+      </ul>
+    </div>
   );
 }
