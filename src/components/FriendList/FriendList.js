70e3a0aa450c2694cb852ab6b0465caa22502a4c
Sebastian Lalek <chemiczq@gmail.com>
2023-10-18 16:59:30 +0200

    update FriendList

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -3,17 +3,28 @@ import css from './FriendList.module.css';
 export default function FriendList({ friends }) {
   return (
     <ul className={css.friend_list}>
-      {friends.map(friend => (
-        <li key={friend.id} className={css.item}>
-          {friend.isOnline ? (
-            <span className={css.online}></span>
-          ) : (
-            <span className={css.offline}></span>
-          )}
-          <img className={css.avatar} src={friend.avatar} alt="" width={48} />
-          <p className={css.name}>{friend.name}</p>
-        </li>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          id={id}
+        />
       ))}
     </ul>
+  );
+}
+
+function FriendListItem({ avatar, name, isOnline, id }) {
+  return (
+    <li key={id} className={css.item}>
+      {isOnline ? (
+        <span className={css.online}></span>
+      ) : (
+        <span className={css.offline}></span>
+      )}
+      <img className={css.avatar} src={avatar} alt="" width={48} />
+      <p className={css.name}>{name}</p>
+    </li>
   );
 }
